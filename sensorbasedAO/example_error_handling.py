f0a3e604f02c2f72fd997b9cef7767a76a10732b
Jiahe Cui <jesu3424@eng.ox.ac.uk>
2019-09-16 10:19:54 +0100

    Initial setup 5

---
+++ sensorbasedAO/example_error_handling.py
@@ -0,0 +1,54 @@
+from ximea import xiapi
+import numpy as np
+
+# create instance for first connected camera
+cam = xiapi.Camera()
+
+# start camera communication
+print('Opening camera...')
+cam.open_device_by_SN( '26883050')
+
+# camera settings
+cam.set_imgdataformat('XI_MONO8')
+cam.set_exposure(4000)
+print('Exposure set to %i us' % cam.get_exposure())
+
+# create instance of Image to store image data and metadata
+img = xiapi.Image()
+dataimages = np.zeros((2048,2048))
+
+# start data acquisition
+print('Starting data acquisition...')
+cam.start_acquisition()
+
+# get data and pass them from camera to img
+# if timeout error is raised, print it and continue
+try:
+    cam.get_image(img, timeout=10)
+
+    # get raw data from camera
+    # for Python3.x function returns bytes
+    dataimages = img.get_image_data_numpy()
+
+    #print image data and metadata
+    print('Image width (pixels):  ' + str(img.width))
+    print('Image height (pixels): ' + str(img.height))
+    print('First line of pixels: ' + str(dataimages[0]))
+    
+except xiapi.Xi_error as err:
+    if err.status == 10:
+        print('Timeout error occured.')
+    else:
+        raise
+
+#stop data acquisition
+print('Stopping acquisition...')
+cam.stop_acquisition()
+
+#stop communication
+cam.close_device()
+
+print('Done.')
+
+
+
