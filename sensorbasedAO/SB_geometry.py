64589ccc1a15a89734f25df7d641ba0ce224c64d
Jiahe Cui <jiahe.cui@eng.ox.ac.uk>
2019-11-18 19:09:28 +0000

    Comments added for sensor initialisation

--- sensorbasedAO/SB_geometry.py
+++ sensorbasedAO/SB_geometry.py
@@ -117,8 +117,8 @@ class Setup_SB(QObject):
             self.SB_layer_2D[SB_offset_y : SB_final_y, ref_column_outline] = self.outline_int
 
             # Display search blocks and reference centroids
-            im = PIL.Image.fromarray(self.SB_layer_2D, 'L')
-            im.show()
+            # im = PIL.Image.fromarray(self.SB_layer_2D, 'L')
+            # im.show()
             self.layer.emit(self.SB_layer_2D)
             time.sleep(1)
 
@@ -239,14 +239,14 @@ class Setup_SB(QObject):
             self.SB_layer_2D[x, y] = self.outline_int
 
             # Display actual search blocks and reference centroids
-            im = PIL.Image.fromarray(self.SB_layer_2D, 'L')
-            im.show()
+            # im = PIL.Image.fromarray(self.SB_layer_2D, 'L')
+            # im.show()
             self.layer.emit(self.SB_layer_2D)
 
             # Get actual search block coordinates
             self.act_SB_coord = np.nonzero(np.ravel(self.SB_layer_2D))
             self.act_SB_coord = np.array(self.act_SB_coord)
-            os.system('pause')
+
             """
             Returns search block information
             """
--- sensorbasedAO/sensor.py
+++ sensorbasedAO/sensor.py
@@ -16,24 +16,29 @@ class SENSOR_XIMEA(xiapi.Camera):
     Create an instance of the Camera class to access its methods, attributes, and the target device at serial port.
     """
     def __init__(self):
+        # Create Camera instance
         self.sensor = xiapi.Camera()
+
+        # Open camera by serial number
         self.sensor.open_device_by_SN(config['camera']['SN'])
         
+        # Set camera image format
         self.sensor.set_imgdataformat(config['camera']['dataformat'])
 
+        # Set camera exposure and gain preference
         if config['camera']['auto_gain'] == 0:
             self.sensor.set_exposure(config['camera']['exposure'])
         else:
             self.sensor.enable_aeag()
             self.sensor.set_exp_priority(config['camera']['exp_priority'])
             self.sensor.set_ae_max_limit(config['camera']['ae_max_limit'])
-       
+
+        # Set camera frame acquisition mode       
         if config['camera']['burst_mode'] == 1:
             self.sensor.set_trigger_selector('XI_TRG_SEL_FRAME_BURST_START')
             self.sensor.set_acq_frame_burst_count(config['camera']['burst_frames'])
 
         self.sensor.set_acq_timing_mode(config['camera']['acq_timing_mode'])
-        # self.sensor.set_framerate(config['camera']['frame_rate'])
 
         super().__init__()
 
