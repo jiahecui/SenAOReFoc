a22d54a2ddaf2ebb7c24783d291115f67f23034f
Jiahe Cui <jesu3424@eng.ox.ac.uk>
2019-11-23 09:43:09 +0000

    Dimensional bug fixed in spot_sim.py

--- sensorbasedAO/spot_sim.py
+++ sensorbasedAO/spot_sim.py
@@ -1,87 +1,100 @@
 import numpy as np
 import PIL.Image
+import random
 
-def SH_spot_sim(settings):
+class SpotSim():
     """
     Generates a simulated S-H spot image
     """
-    # Initialise spot image array
-    SH_spot_img = np.zeros([settings['sensor_width'], settings['sensor_height']])
+    def __init__(self, settings):
+        
+        # Get sensor and search block parameters
+        self.settings = settings
 
-    # Use actual search block reference centroid coords as upper left corner position for tiling of individual S-H spots
-    offset_coord = settings['act_ref_cent_coord']
+    def SH_spot_sim(self):
+        """
+        Generates a simulated S-H spot image
+        """
+        # Initialise spot image array
+        self.SH_spot_img = np.zeros([self.settings['sensor_width'], self.settings['sensor_height']])
 
-    # Get other non-variable parameters for generation of a Gaussian profile S-H spot
-    sigma = 0.1
-    array_size = settings['SB_diam']
-    x = np.linspace(-1, 1, array_size)
-    y = np.linspace(-1, 1, array_size)
+        # Use actual search block reference centroid coords as upper left corner position for tiling of individual S-H spots
+        offset_coord = self.settings['act_ref_cent_coord']
 
-    # Generate individual Gaussian profile S-H spots and integrate them to pre-initialised spot image array
-    for i in range(len(offset_coord)):
+        # Get other non-variable parameters for generation of a Gaussian profile S-H spot
+        sigma = 0.1
+        array_size = self.settings['SB_diam']
+        x = np.linspace(-1, 1, array_size)
+        y = np.linspace(-1, 1, array_size)
 
-        # Generate random positions for centre of each S-H spot
-        xc = round(random.uniform(-0.5, 0.5), 2)
-        yc = round(random.uniform(-0.5, 0.5), 2)
+        # Generate individual Gaussian profile S-H spots and integrate them to pre-initialised spot image array
+        for i in range(len(offset_coord)):
+  
+            # Generate random positions for centre of each S-H spot
+            xc = round(random.uniform(-0.5, 0.5), 2)
+            yc = round(random.uniform(-0.5, 0.5), 2)
 
-        # Generate a Gaussian profile S-H spot
-        Gaus_spot = dirac_function(x, y, xc, yc, sigma, array_size)
+            # Generate a Gaussian profile S-H spot
+            Gaus_spot = self.dirac_function(x, y, xc, yc, sigma, array_size)
 
-        # Integrate it to pre-initialised spot image array
-        A_start = [0, 0]
-        B_start = [offset_coord[i] // settings['sensor_width'], offset_coord[i] % settings['sensor_width']]
-        B_end = [offset_coord[i] // settings['sensor_width'] + array_size, offset_coord[i] % settings['sensor_width'] + array_size]
-        SH_spot_img = array_integrate(Gaus_spot, SH_spot_img, A_start, B_start, B_end)
+            # Integrate it to pre-initialised spot image array
+            A_start = [0, 0]
+            B_start = [offset_coord[i] // self.settings['sensor_width'], offset_coord[i] % self.settings['sensor_width']]
+            B_end = [offset_coord[i] // self.settings['sensor_width'] + array_size, offset_coord[i] % self.settings['sensor_width'] + array_size]
 
-    return SH_spot_img
+            self.SH_spot_img = self.array_integrate(Gaus_spot, self.SH_spot_img, A_start, B_start, B_end)
 
-def dirac_function(x, y, xc, yc, sigma, size):
-    """
-    Generates a 2D Gaussian profile array
+        return self.SH_spot_img
 
-    Args:
-        x: 1D vector of positions along x axis
-        y: 1D vector of positions along y axis
-        xc: centre of profile along x axis
-        yc: centre of profile along y axis
-        sigma: standard deviation of Gaussian profile
-        size: size of Gaussian profile array
-    """
-    # Initialise Gaussian profile array
-    GaussProf = np.zeros([size, size])
+    def dirac_function(self, x, y, xc, yc, sigma, size):
+        """
+        Generates a 2D Gaussian profile array
 
-    # Generate Gaussian profile
-    for i in range(size):
-        for j in range(size):
-            GaussValue = (255 * np.exp( - ((x[j] - xc) ** 2 + (y[i] - yc) ** 2) / (2 *sigma ** 2))).astype(np.uint8)
-            GaussProf[j, i] = GaussValue
+        Args:
+            x: 1D vector of positions along x axis
+            y: 1D vector of positions along y axis
+            xc: centre of profile along x axis
+            yc: centre of profile along y axis
+            sigma: standard deviation of Gaussian profile
+            size: size of Gaussian profile array
+        """
 
-    return GaussProf
+        # Initialise Gaussian profile array
+        GaussProf = np.zeros([size, size])
 
-def array_integrate(A, B, A_start, B_start, B_end):
-    """
-    Integrates array A into array B and returns array B
+        # Generate Gaussian profile
+        for i in range(size):
+            for j in range(size):
+                GaussValue = (255 * np.exp( - ((x[j] - xc) ** 2 + (y[i] - yc) ** 2) / (2 *sigma ** 2))).astype(np.uint8)
+                GaussProf[j, i] = GaussValue
 
-    Args:
-        A as numpy array
-        B as numpy array
-        A_start: index with respect to A of the upper left corner of the overlap
-        B_start: index with respect to B of the upper left corner of the overlap
-        B_end：index with respect to B of the lower right corner of the overlap
-    """
-    # Convert coordinate list to 1D array to facilitate calculation
-    A_start, B_start, B_end = map(np.asarray, [A_start, B_start, B_end])
+        return GaussProf
 
-    # Get shape of overlapping part
-    shape = B_end - B_start
+    def array_integrate(self, A, B, A_start, B_start, B_end):
+        """
+        Integrates array A into array B and returns array B
 
-    # Get slice coords of overlapping part relative to both A and B, i.e. B_slices returns
-    # (slice(6, 16, None), slice(0, 10, None)) and A_slices returns (slice(11, 21, None), slice(5, 15, None)) \
-    # means overlapping part stretches from (6, 0) to (16, 10) in B and from (11, 5) and (21, 15) in A
-    B_slices = tuple(map(slice, B_start, B_end + 1))   
-    A_slices = tuple(map(slice, A_start, A_start + shape + 1))
+        Args:
+            A as numpy array
+            B as numpy array
+            A_start: index with respect to A of the upper left corner of the overlap
+            B_start: index with respect to B of the upper left corner of the overlap
+            B_end：index with respect to B of the lower right corner of the overlap
+        """
 
-    # Replace corresponding elements in B with that in A
-    B[B_slices] = A[A_slices]
+        # Convert coordinate list to 1D array to facilitate calculation
+        A_start, B_start, B_end = map(np.asarray, [A_start, B_start, B_end])
+ 
+        # Get shape of overlapping part
+        shape = B_end - B_start
 
-    return B
+        # Get slice coords of overlapping part relative to both A and B, i.e. B_slices returns
+        # (slice(6, 16, None), slice(0, 10, None)) and A_slices returns (slice(11, 21, None), slice(5, 15, None)) \
+        # means overlapping part stretches from (6, 0) to (16, 10) in B and from (11, 5) and (21, 15) in A
+        B_slices = tuple(map(slice, B_start, B_end))   
+        A_slices = tuple(map(slice, A_start, A_start + shape))
+
+        # Replace corresponding elements in B with that in A
+        B[B_slices] = A[A_slices]
+
+        return B
