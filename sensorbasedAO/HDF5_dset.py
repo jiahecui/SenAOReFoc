487425eeb8c344761f6578e09cf291d5c739c663
Jiahe Cui <jiahe.cui@eng.ox.ac.uk>
2019-12-24 12:50:30 +0000

    Function added for loading .mat file and converting phase information to numpy array

--- sensorbasedAO/HDF5_dset.py
+++ sensorbasedAO/HDF5_dset.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 from config import config
 
 def make_dset(group, name, data):
@@ -9,3 +10,10 @@ def dset_append(group, name, data):
     shape[0] += 1
     group[name].resize(shape)
     group[name][-1, ...] = data
+
+def get_mat_dset():
+    f = h5py.File('sensorbasedAO/WrappedPhase_IMG_Blastocyte.mat','r')
+    data = f.get('WrappedPhase')
+    data = np.array(data[-1,...])
+
+    return data
