006417cb90321269537fed3317d5bffa481e0956
Jiahe Cui <jiahe.cui@eng.ox.ac.uk>
2020-01-06 15:48:55 +0000

    Script added to calculate value of deformation introduced by DM at each pixel and the local derivative by modeling the influence function of each actuator as a Gaussian function for simulation of the control matrix of the DM

---
+++ sensorbasedAO/gaussian_inf.py
@@ -0,0 +1,61 @@
+import numpy as np
+from config import config
+
+def inf(xx, yy, xc, yc, j, act_diam):
+    """
+    Generates the value of Gaussian distribution influence function at each pixel
+
+    Args:
+        xx - x value meshgrid
+        yy - y value meshgrid
+        xc - actuator x position array
+        yc - actuator y position array
+        j - actuator index, starts from 0
+        act_diam - actuator diameter in pixels
+    """
+    # Calculate influence function value at each pixel introduced by actuator j
+    Fn = np.exp(np.log(config['DM']['coupling_fac']) * ((xx - xc[j]) ** 2 + (yy - yc[j]) ** 2) / act_diam ** 2)
+
+    return Fn
+
+def inf_diff(xx, yy, xc, yc, j, act_diam, x_flag = True):
+    """
+    Generates the slopes of modeled Gaussian distribution influence function for simulation of DM control matrix
+    
+    Args:
+        xx - x value array
+        yy - y value array
+        xc - actuator x position array
+        yc - actuator y position array
+        j - actuator index, starts from 0
+        act_diam - actuator diameter in pixels
+        x_flag - flag for differentiating relative to x or y
+    """
+    # Initialise instance variables
+    count = 0
+    diff_sum = 0
+
+    # Calculate averaged derivative of the jth actuator influence function
+    for y in yy:
+        for x in xx:
+
+            count += 1
+
+            # Calculate exponential factor
+            expo = np.exp(np.log(config['DM']['coupling_fac']) * ((x - xc[j]) ** 2 + (y - yc[j]) ** 2) / act_diam ** 2)
+
+            # Calculate x or y derivative of Gaussian distribution influence function and get final result
+            if x_flag:
+                dFn_dx = 2 * np.log(config['DM']['coupling_fac']) / act_diam ** 2 * (x - xc[j])
+                dFn = dFn_dx * expo
+            else:
+                dFn_dy = 2 * np.log(config['DM']['coupling_fac']) / act_diam ** 2 * (y - yc[j])
+                dFn = dFn_dy * expo
+
+            # Sum derivative for each element in search block
+            diff_sum += dFn
+    
+    # Divide sum of derivative by total number of counts:
+    dFn_ave = diff_sum / count
+
+    return dFn_ave
