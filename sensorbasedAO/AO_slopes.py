Defocus incorporated into strehl ratio calculation during AO correction loops, miscellaneous bugs fixed, sharpness threshold changed from 0.002 to 0.001 according to experimental data, 'AO_zernikes.py', 'AO_slopes.py', 'AO_zernikes_test.py' and remote focusing loops all tested to be working except that the scanner doesn't seem to be moving in mode 2 of 'AO_zernikes_test.py' (same aberration value for all scan points)

sensorbasedAO/AO_slopes.py
@@ -324,7 +324,7 @@ class AO_Slopes(QObject):
                         # Get phase residual (zernike coefficient residual error) and calculate root mean square (rms) error
                         zern_err = self.zern_coeff_detect.copy()
                         zern_err_part = self.zern_coeff_detect.copy()
-                        zern_err_part[[0, 1, 3], 0] = 0
+                        zern_err_part[[0, 1], 0] = 0
                         rms_zern = np.sqrt((zern_err ** 2).sum())
                         rms_zern_part = np.sqrt((zern_err_part ** 2).sum())
                         self.loop_rms_zern[i] = rms_zern
@@ -610,7 +610,7 @@ class AO_Slopes(QObject):
                         # Get phase residual (zernike coefficient residual error) and calculate root mean square (rms) error
                         zern_err = self.zern_coeff_detect.copy()
                         zern_err_part = self.zern_coeff_detect.copy()
-                        zern_err_part[[0, 1, 3], 0] = 0
+                        zern_err_part[[0, 1], 0] = 0
                         rms_zern = np.sqrt((zern_err ** 2).sum())
                         rms_zern_part = np.sqrt((zern_err_part ** 2).sum())
                         self.loop_rms_zern[i] = rms_zern
@@ -900,7 +900,7 @@ class AO_Slopes(QObject):
                             # Get phase residual (zernike coefficient residual error) and calculate root mean square (rms) error
                             zern_err = self.zern_coeff_detect.copy()
                             zern_err_part = self.zern_coeff_detect.copy()
-                            zern_err_part[[0, 1, 3], 0] = 0
+                            zern_err_part[[0, 1], 0] = 0
                             rms_zern = np.sqrt((zern_err ** 2).sum())
                             rms_zern_part = np.sqrt((zern_err_part ** 2).sum())
                             self.loop_rms_zern[i,j] = rms_zern
@@ -1238,7 +1238,7 @@ class AO_Slopes(QObject):
                             # Get phase residual (zernike coefficient residual error) and calculate root mean square (rms) error
                             zern_err = self.zern_coeff_detect.copy()
                             zern_err_part = self.zern_coeff_detect.copy()
-                            zern_err_part[[0, 1, 3], 0] = 0
+                            zern_err_part[[0, 1], 0] = 0
                             rms_zern = np.sqrt((zern_err ** 2).sum())
                             rms_zern_part = np.sqrt((zern_err_part ** 2).sum())
                             self.loop_rms_zern[i,j] = rms_zern

sensorbasedAO/AO_zernikes_test.py
@@ -655,7 +655,7 @@ class AO_Zernikes_Test(QObject):
                     if self.loop:
                         
                         try:
-
+                            
                             # Send voltages to scanner
                             self.scanner.GoToDevicePosition(x_array[m], 0, 255, 5)
                         
@@ -684,9 +684,9 @@ class AO_Zernikes_Test(QObject):
                             # Get phase residual (zernike coefficient residual error) and calculate root mean square (rms) error
                             zern_err = self.zern_coeff_detect.copy()
                             rms_zern = np.sqrt((zern_err ** 2).sum())
-                            self.zern_x[:,m,l] = zern_err
+                            self.zern_x[:,m,l] = zern_err[:,0]
 
-                            print('Full zernike root mean square error {} is {} um'.format(i, rms_zern))                              
+                            print('Full zernike root mean square error {} is {} um'.format(l, rms_zern))                              
 
                         except Exception as e:
                             print(e)
@@ -735,9 +735,9 @@ class AO_Zernikes_Test(QObject):
                             # Get phase residual (zernike coefficient residual error) and calculate root mean square (rms) error
                             zern_err = self.zern_coeff_detect.copy()
                             rms_zern = np.sqrt((zern_err ** 2).sum())
-                            self.zern_y[:,m,l] = zern_err
+                            self.zern_y[:,m,l] = zern_err[:,0]
 
-                            print('Full zernike root mean square error {} is {} um'.format(i, rms_zern))                              
+                            print('Full zernike root mean square error {} is {} um'.format(l, rms_zern))                              
 
                         except Exception as e:
                             print(e)

sensorbasedAO/calibration_RF.py
@@ -76,7 +76,7 @@ class Calibration_RF(QObject):
             voltages = np.zeros(self.actuator_num)
 
             # Iterate through each step and retrieve the voltages that correct for the aberration at that step
-            for i in range(config['RF_calib']['step_num']):
+            # for i in range(config['RF_calib']['step_num']):
 
         except Exception as e:
             raise

sensorbasedAO/image_acquisition.py
@@ -61,7 +61,7 @@ def acq_image(sensor, height, width, acq_mode = 0):
                 sensor.get_image(img, timeout = 25)
 
                 prev2 = time.perf_counter()
-                print('Time for acquisition of frame {} is: {} s'.format((i + 1), (prev2 - prev1)))
+                # print('Time for acquisition of frame {} is: {} s'.format((i + 1), (prev2 - prev1)))
 
                 # Create numpy array with data from camera, dimensions are determined by imgdataformats
                 dataimage = img.get_image_data_numpy()
@@ -81,7 +81,7 @@ def acq_image(sensor, height, width, acq_mode = 0):
                     raise
 
             prev3 = time.perf_counter()
-            print('Time for acquisition of loop {} is: {} s'.format((i + 1), (prev3 - prev1)))
+            # print('Time for acquisition of loop {} is: {} s'.format((i + 1), (prev3 - prev1)))
 
         print('Length of data list is:', np.shape(data)[2])
 