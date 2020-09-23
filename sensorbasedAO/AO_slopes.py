4a7cb80fc976957fd5f1015bcd48093a23a6057b
Jiahe Cui <jiahe.cui@eng.ox.ac.uk>
2020-09-23 02:02:05 +0100

    Bug fix for previous commit

--- sensorbasedAO/AO_slopes.py
+++ sensorbasedAO/AO_slopes.py
@@ -742,11 +742,11 @@ class AO_Slopes(QObject):
                 if self.AO_settings['focus_enable'] == 1:
                     if self.focus_settings['focus_mode_flag'] == 0:
                         RF_index = int(self.focus_settings['focus_depth_defoc'] // config['RF_calib']['step_incre'])
-                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, self.correct_num - 1 - RF_index])
+                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, config['RF_calib']['step_num'] - 1 - RF_index])
                     else:
                         RF_index = int(self.focus_settings['start_depth_defoc'] // config['RF_calib']['step_incre'] \
                             + self.focus_settings['step_incre_defoc'] // config['RF_calib']['step_incre'] * j)
-                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, self.correct_num - 1 - RF_index])
+                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, config['RF_calib']['step_num'] - 1 - RF_index])
                 else:
                     voltages_defoc = 0
 
@@ -1051,11 +1051,11 @@ class AO_Slopes(QObject):
                 if self.AO_settings['focus_enable'] == 1:
                     if self.focus_settings['focus_mode_flag'] == 0:
                         RF_index = self.focus_settings['focus_depth_defoc'] // config['RF_calib']['step_incre']
-                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, self.correct_num - 1 - RF_index])
+                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, config['RF_calib']['step_num'] - 1 - RF_index])
                     else:
                         RF_index = self.focus_settings['start_depth_defoc'] // config['RF_calib']['step_incre'] \
                             + self.focus_settings['step_incre_defoc'] // config['RF_calib']['step_incre'] * j
-                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, self.correct_num - 1 - RF_index])
+                        voltages_defoc = np.ravel(self.mirror_settings['remote_focus_voltages'][:, config['RF_calib']['step_num'] - 1 - RF_index])
                 else:
                     voltages_defoc = 0
 
